cedb3a4be09f51034a7bc11b190f8a607d77ccf2
bob Luse <boblusebob@aim.com>
2022-06-13 09:50:19 -0500

    Changes to the readability of the cards - font and line width.

--- card.py
+++ card.py
@@ -41,45 +41,45 @@ class Card:
 
         # clear rectangle
         canvas.create_rectangle(x, y, x+Card.width, y+Card.height, fill="white")
-        # draw border
+        # .color() border
         canvas.create_rectangle(x, y, x+Card.width, y+Card.height, outline="blue")
 
         # draw body of card
         # get the color, red or black
 
         if self.faceUp():
-            canvas.create_text(x + 5, y + 15, text=names[self.rank()], fill=self.color())
+            canvas.create_text(x + 5, y + 15, text=names[self.rank()], font="Times 20 italic bold", fill=self.color())
             if self.suit() == self.heart:
-                canvas.create_line(x + 25, y + 30, x + 35, y + 20, fill=self.color())
-                canvas.create_line(x + 35, y + 20, x + 45, y + 30, fill=self.color())
-                canvas.create_line(x + 45, y + 30, x + 25, y + 60, fill=self.color())
-                canvas.create_line(x + 25, y + 60, x + 5, y + 30, fill=self.color())
-                canvas.create_line(x + 5, y + 30, x + 15, y + 20, fill=self.color())
-                canvas.create_line(x + 15, y + 20, x + 25, y + 30, fill=self.color())
+                canvas.create_line(x + 25, y + 30, x + 35, y + 20, fill=self.color(), width=2)
+                canvas.create_line(x + 35, y + 20, x + 45, y + 30, fill=self.color(), width=2)
+                canvas.create_line(x + 45, y + 30, x + 25, y + 60, fill=self.color(), width=2)
+                canvas.create_line(x + 25, y + 60, x + 5, y + 30, fill=self.color(), width=2)
+                canvas.create_line(x + 5, y + 30, x + 15, y + 20, fill=self.color(), width=2)
+                canvas.create_line(x + 15, y + 20, x + 25, y + 30, fill=self.color(), width=2)
             elif self.suit() == self.spade:
-                canvas.create_line(x + 15, y + 45, x + 25, y + 25, fill=self.color())
-                canvas.create_line(x + 25, y + 25, x + 35, y + 45, fill=self.color())
-                canvas.create_line(x + 15, y + 45, x + 35, y + 45, fill=self.color())
+                canvas.create_line(x + 15, y + 45, x + 25, y + 25, fill=self.color(), width=2)
+                canvas.create_line(x + 25, y + 25, x + 35, y + 45, fill=self.color(), width=2)
+                canvas.create_line(x + 15, y + 45, x + 35, y + 45, fill=self.color(), width=2)
                 # Base
-                canvas.create_line(x + 23, y + 45, x + 20, y + 55, fill=self.color())
-                canvas.create_line(x + 20, y + 55, x + 30, y + 55, fill=self.color())
-                canvas.create_line(x + 30, y + 55, x + 27, y + 45, fill=self.color())
+                canvas.create_line(x + 23, y + 45, x + 20, y + 55, fill=self.color(), width=2)
+                canvas.create_line(x + 20, y + 55, x + 30, y + 55, fill=self.color(), width=2)
+                canvas.create_line(x + 30, y + 55, x + 27, y + 45, fill=self.color(), width=2)
             elif self.suit() == self.diamond:
-                canvas.create_line(x + 10, y + 35, x + 25, y + 15, fill=self.color())
-                canvas.create_line(x + 25, y + 15, x + 40, y + 35, fill=self.color())
-                canvas.create_line(x + 40, y + 35, x + 25, y + 55, fill=self.color())
-                canvas.create_line(x + 25, y + 55, x + 10, y + 35, fill=self.color())
+                canvas.create_line(x + 10, y + 35, x + 25, y + 15, fill=self.color(), width=2)
+                canvas.create_line(x + 25, y + 15, x + 40, y + 35, fill=self.color(), width=2)
+                canvas.create_line(x + 40, y + 35, x + 25, y + 55, fill=self.color(), width=2)
+                canvas.create_line(x + 25, y + 55, x + 10, y + 35, fill=self.color(), width=2)
             elif self.suit() == self.club:
-                canvas.create_oval(x + 20, y + 25, x+30, y+35, outline=self.color())
-                canvas.create_oval(x + 25, y + 35, x+35, y+45, outline=self.color())
-                canvas.create_oval(x + 15, y + 35, x+35, y+45, outline=self.color())
+                canvas.create_oval(x + 20, y + 25, x+30, y+35, outline=self.color(), width=2)
+                canvas.create_oval(x + 25, y + 35, x+35, y+45, outline=self.color(), width=2)
+                canvas.create_oval(x + 15, y + 35, x+35, y+45, outline=self.color(), width=2)
                 # Base
-                canvas.create_line(x + 23, y + 45, x + 20, y + 55, fill=self.color())
-                canvas.create_line(x + 20, y + 55, x + 30, y + 55, fill=self.color())
-                canvas.create_line(x + 30, y + 55, x + 27, y + 45, fill=self.color())
+                canvas.create_line(x + 23, y + 45, x + 20, y + 55, fill=self.color(), width=2)
+                canvas.create_line(x + 20, y + 55, x + 30, y + 55, fill=self.color(), width=2)
+                canvas.create_line(x + 30, y + 55, x + 27, y + 45, fill=self.color(), width=2)
         else:  # face down
-            canvas.create_line(x + 15, y + 5, x + 15, y + 65, fill=self.color())
-            canvas.create_line(x + 35, y + 5, x + 35, y + 65, fill=self.color())
-            canvas.create_line(x + 5, y + 20, x + 45, y + 20, fill=self.color())
-            canvas.create_line(x + 5, y + 35, x + 45, y + 35, fill=self.color())
-            canvas.create_line(x + 5, y + 50, x + 45, y + 50, fill=self.color())
+            canvas.create_line(x + 15, y + 5, x + 15, y + 65, fill=self.color(), width=2)
+            canvas.create_line(x + 35, y + 5, x + 35, y + 65, fill=self.color(), width=2)
+            canvas.create_line(x + 5, y + 20, x + 45, y + 20, fill=self.color(), width=2)
+            canvas.create_line(x + 5, y + 35, x + 45, y + 35, fill=self.color(), width=2)
+            canvas.create_line(x + 5, y + 50, x + 45, y + 50, fill=self.color(), width=2)
